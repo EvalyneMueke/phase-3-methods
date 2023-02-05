a5064f588327e1ba21fa897d9c7e1354a3f79132
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-02-05 17:05:26 +0300

    Done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+
+end
+
+greet_programmer
+
+def greet (name)
+    puts "Hello, #{name}!"
+
+end
+greet "Naureen"
+
+def greet_with_default (name ="programmer")
+
+    puts "Hello, #{name}!"
+end
+
+greet_with_default"Naureen"
+
+def add (num1, num2)
+    return num1 + num2
+
+end
+
+add(1, 2)
+
+def halve(number)
+    number.is_a?(Integer) ? number / 2 : nil
+end
+
+halve(6)
+
+
+
+
